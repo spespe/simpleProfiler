6df2bf18f710468828d63603e8d60df6a623a0a4
Pietro.Speri <speri.pietro@gmail.com>
2019-01-17 12:57:34 -0800

    changed logs and return

--- src/main/scala/spark/simpleProfiler.scala
+++ src/main/scala/spark/simpleProfiler.scala
@@ -2,9 +2,6 @@ package spark
 
 import java.util.Date
 import java.util.concurrent.TimeUnit
-
-import org.apache.spark.rdd
-//import com.typesafe.scalalogging.slf4j.LazyLogging
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
 import org.apache.spark.storage.StorageLevel
@@ -16,17 +13,14 @@ object profiling {
   def main(args: Array[String]) {
     //Start time
     val startTime= new Date
-    println("[START TIME: ]"+startTime)
 
     //Inputs
     val db=args(0).trim
     val table=args(1).trim
     val column=args(2).trim
     val cache=args(3).trim
-    args.foreach(x=>println("ARGUMENT: "+x))
 
     //Spark configuration
-    println("[CONFIGURING SPARK CONTEXT AND SQL CONTEXT]")
     val conf = new org.apache.spark.SparkConf().setAppName("Profiler")
     val sc = new SparkContext(conf)
     val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
@@ -42,7 +36,6 @@ object profiling {
 
     def profiler(db: => String)(table: => String)(col: => String)(cache: =>String)={
       val sep="|"
-      println("[CREATING RDD]")
       def rddGen(cache:String)= {
         cache.trim.toUpperCase match {
           case "N" => sqlContext.table(db + "." + table).select(col).rdd
@@ -50,26 +43,18 @@ object profiling {
         }
       }
       val rdd=rddGen(cache)
-      println("[RDD COUNT]")
       val count=rdd.count
-      println("[RDD DISTINCT COUNT]")
       val distinctCount=rdd.distinct.count
       val isDistinct=if(count==distinctCount)"TRUE" else "FALSE"
-      println("[BLANKS COUNT]")
       val blanks=rdd.filter(x=>x.mkString.isEmpty).count
       val withBlanks=if(blanks==0)"FALSE" else "TRUE"
-      println("[NULLS COUNT]")
       val hasNull=rdd.filter(x=>x!=null).isEmpty.toString.toUpperCase
-      println("[MAX LENGTH CHECK]")
       val maxLength=rdd.reduce((a,b)=>if(a.mkString.length>b.mkString.length) a else b).mkString.length
-      println("[MIN LENGTH CHECK]")
       val minLength=rdd.reduce((a,b)=>if(a.mkString.length<b.mkString.length) a else b).mkString.length
-      println("[UNPERSISTING RDD]")
       rdd.unpersist()
-      db+sep+table+sep+col+sep+isDistinct+sep+withBlanks+sep+hasNull+sep+maxLength+sep+minLength
+      println(db+sep+table+sep+col+sep+isDistinct+sep+withBlanks+sep+hasNull+sep+maxLength+sep+minLength)
     }
 
-    println("[LAUNCHING THE PROFILER]")
     profiler(db.mkString)(table.mkString)(column.mkString)(cache.mkString)
 
     //val endTime= new Date
