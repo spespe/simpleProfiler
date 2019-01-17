4e9e36e44b2e9581636979eaae89ad89b8dbb4dd
Pietro.Speri <speri.pietro@gmail.com>
2019-01-17 10:38:00 -0800

    changed the profiler method to add cache argument

--- src/main/scala/spark/simpleProfiler.scala
+++ src/main/scala/spark/simpleProfiler.scala
@@ -2,7 +2,9 @@ package spark
 
 import java.util.Date
 import java.util.concurrent.TimeUnit
-import com.typesafe.scalalogging.LazyLogging
+
+import org.apache.spark.rdd
+//import com.typesafe.scalalogging.slf4j.LazyLogging
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
 import org.apache.spark.storage.StorageLevel
@@ -10,20 +12,21 @@ import org.apache.spark.storage.StorageLevel
 /**
   * Created by Pietro.Speri on 12/01/2019.
   */
-object simpleProfiler extends LazyLogging with functions {
+object profiling {
   def main(args: Array[String]) {
     //Start time
     val startTime= new Date
-    logger.info("[START TIME: ]"+startTime)
+    println("[START TIME: ]"+startTime)
 
     //Inputs
     val db=args(0).trim
     val table=args(1).trim
     val column=args(2).trim
-    logger.info("[ARGUMENTS PASSED: ]"+args.foreach(println))
+    val cache=args(3).trim
+    args.foreach(x=>println("ARGUMENT: "+x))
 
     //Spark configuration
-    logger.info("[CONFIGURING SPARK CONTEXT AND SQL CONTEXT]")
+    println("[CONFIGURING SPARK CONTEXT AND SQL CONTEXT]")
     val conf = new org.apache.spark.SparkConf().setAppName("Profiler")
     val sc = new SparkContext(conf)
     val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
@@ -32,38 +35,49 @@ object simpleProfiler extends LazyLogging with functions {
     Logger.getLogger("akka").setLevel(Level.OFF)
     Logger.getLogger("org").setLevel(Level.OFF)
 
-    def profiler(db: => String)(table: => String)(col: => String)={
+    //def getDateDiff(date1:Date,date2:Date,timeUnit:TimeUnit)= {
+    //  val diffInSeconds = date2.getTime() - date1.getTime()
+    //  timeUnit.convert(diffInSeconds,TimeUnit.SECONDS)
+    //}
+
+    def profiler(db: => String)(table: => String)(col: => String, cache: =>String)={
       val sep="|"
-      logger.info("[CREATING RDD]")
-      val rdd=sqlContext.table(db+"."+table).select(col).rdd.persist(StorageLevel.MEMORY_AND_DISK)
-      logger.info("[RDD COUNT]")
+      println("[CREATING RDD]")
+      def rddGen(cache:String)= {
+        cache.trim.toUpperCase match {
+          case "Y" => sqlContext.table(db + "." + table).select(col).rdd.persist(StorageLevel.MEMORY_AND_DISK)
+          case "N" => sqlContext.table(db + "." + table).select(col).rdd
+        }
+      }
+      val rdd=rddGen(cache)
+      println("[RDD COUNT]")
       val count=rdd.count
-      logger.info("[RDD DISTINCT COUNT]")
+      println("[RDD DISTINCT COUNT]")
       val distinctCount=rdd.distinct.count
       val isDistinct=if(count==distinctCount)"TRUE" else "FALSE"
-      logger.info("[BLANKS COUNT]")
+      println("[BLANKS COUNT]")
       val blanks=rdd.filter(x=>x.mkString.isEmpty).count
       val withBlanks=if(blanks==0)"FALSE" else "TRUE"
-      logger.info("[NULLS COUNT]")
+      println("[NULLS COUNT]")
       val hasNull=rdd.filter(x=>x!=null).isEmpty.toString.toUpperCase
-      logger.info("[MAX LENGTH CHECK]")
+      println("[MAX LENGTH CHECK]")
       val maxLength=rdd.reduce((a,b)=>if(a.mkString.length>b.mkString.length) a else b).mkString.length
-      logger.info("[MIN LENGTH CHECK]")
+      println("[MIN LENGTH CHECK]")
       val minLength=rdd.reduce((a,b)=>if(a.mkString.length<b.mkString.length) a else b).mkString.length
-      logger.info("[UNPERSISTING RDD]")
+      println("[UNPERSISTING RDD]")
       rdd.unpersist()
       println(db+sep+table+sep+col+sep+isDistinct+sep+withBlanks+sep+hasNull+sep+maxLength+sep+minLength)
     }
 
-    logger.info("[LAUNCHING THE PROFILER]")
-    profiler(db.mkString)(table.mkString)(column.mkString)
+    println("[LAUNCHING THE PROFILER]")
+    profiler(db.mkString)(table.mkString)(column.mkString,cache.mkString)
 
-    val endTime= new Date
-    val units=TimeUnit.SECONDS
-    val timeTaken=getDateDiff(startTime,endTime,units)
+    //val endTime= new Date
+    //val units=TimeUnit.SECONDS
+    //val timeTaken=getDateDiff(startTime,endTime,units)
 
-    logger.info("[END TIME: ]"+endTime)
-    logger.info("[TIME TAKEN IN SECONDS: ]"+timeTaken)
+    //println("[END TIME: ]"+endTime)
+    //println("[TIME TAKEN IN SECONDS: ]"+timeTaken)
 
     sc.stop
   }
