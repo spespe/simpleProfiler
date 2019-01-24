0abd83406e1ed95c2b4d93944675e1886eed1837
Pietro.Speri <speri.pietro@gmail.com>
2019-01-23 21:01:57 -0800

    Added check on nulls in length

--- src/main/scala/spark/simpleProfiler.scala
+++ src/main/scala/spark/simpleProfiler.scala
@@ -53,8 +53,8 @@ object profiling {
       val blanks=rdd.filter(x=>x.mkString.isEmpty).count
       val withBlanks=if(blanks==0)"FALSE" else "TRUE"
       val hasNull=rdd.filter(x=>x!=null).isEmpty.toString.toUpperCase
-      val maxLength=rdd.reduce((a,b)=>if(a.mkString.length>b.mkString.length) a else b).mkString.length
-      val minLength=rdd.reduce((a,b)=>if(a.mkString.length<b.mkString.length) a else b).mkString.length
+      val maxLength=rdd.reduce((a,b)=>if(a.mkString.length>b.mkString.length && a!=null && b!=null) a else b).mkString.length
+      val minLength=rdd.reduce((a,b)=>if(a.mkString.length<b.mkString.length && a!=null && b!=null) a else b).mkString.length
       rdd.unpersist()
       println(db+sep+table+sep+col+sep+isDistinct+sep+withBlanks+sep+hasNull+sep+maxLength+sep+minLength)
     }
