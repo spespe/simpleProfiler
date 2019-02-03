b82e3171741588c7cf9e70a5f516c58ef473c423
Pietro Speri <speri.pietro@gmail.com>
2019-02-02 19:36:09 -0800

    Updated ssh conn script to grep for pattern and add count to new file
    Retrieving the pattern while connected through ssh, adding the count to a new file in the target server

--- ssh_conn.sh
+++ ssh_conn.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# CREATED BY: PIETRO SPERI
-
 REMOTE_HOST=${1}
+PATTERN=${2}
 USER=`whoami`
 
 KEY=`ssh-keygen`
@@ -10,8 +9,7 @@ COPY=`ssh-copy-id -i ~/.ssh/id_rsa.pub ${REMOTE_HOST}`
 echo "STARTING THE SSH NOW, PLEASE PRESS ENTER, YES AND YOUR PASSWORD WHENEVER REQUIRED"
 
 ssh ${USER}@${REMOTE_HOST} << EOF
-  ls -altr
-  echo "WORKING PERFECTLY"
+  netstat -nl | grep -c ${PATTERN} > pattern_check_${REMOTE_HOST}.txt
   exit
 EOF
 
